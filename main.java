16fdff9a027b853a6b74aa125b0e5a28720db6ba
Denis Moskalenko <96694072+Ulibocka12@users.noreply.github.com>
2021-12-31 21:09:32 +0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,5 @@
+        System.out.println("Сварили суп. На одного человека вышло:");
+        System.out.println((water / eaters) + " миллилитров воды");
+        System.out.println((potatoes / eaters) + " картофелин(а)");
+        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
+        System.out.println((spices / eaters) + " ложек(ка) специй");
